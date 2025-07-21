03415cacf03e60fc9e800abe7ff4ad5613183d51
chen_wei <chen_wei@intsig.net>
2025-07-21 14:27:13 +0800

    添加快捷键命令支持，允许通过Ctrl+K（或Command+K）打开标签页搜索，并在background.js中添加相应的事件监听器。

--- background.js
+++ background.js
@@ -10,3 +10,19 @@ chrome.runtime.onInstalled.addListener(() => {
     }
   });
 });
+
+// 监听快捷键命令
+chrome.commands.onCommand.addListener((command) => {
+  if (command === "_execute_action") {
+    // 打开popup（这个命令会自动触发popup打开）
+    // 由于使用了_execute_action，Chrome会自动打开popup
+    console.log('快捷键被触发');
+  }
+});
+
+// 监听快捷键触发的action点击事件
+chrome.action.onClicked.addListener((tab) => {
+  // 这个监听器在使用popup时通常不会被触发
+  // 但保留以防需要自定义行为
+  console.log('扩展图标被点击');
+});
